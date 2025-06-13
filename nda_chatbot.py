132dfcfc0591ea864f6480763634f980556fb3b8
PaoloRanno1 <paoloranno1@gmail.com>
2025-06-13 16:35:10 +0200

    Update nda_chatbot.py

--- nda_chatbot.py
+++ nda_chatbot.py
@@ -7,7 +7,9 @@ import warnings
 from typing import Dict, Any, List
 
 warnings.filterwarnings("ignore")
-
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 from langchain.document_loaders import PyPDFLoader
 from langchain.vectorstores import Chroma
 from langchain.embeddings.openai import OpenAIEmbeddings
